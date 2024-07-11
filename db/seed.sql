21f2d9a9ec1df7a8a2b1ed8b1ec171f9c6b55133
Brendan O'Brien <bobrien@pursuit.org>
2024-07-11 16:38:18 -0400

    creates seed values

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Something', 'Someone', 'Slow Children At Play', '3:08', FALSE),
+('Keep Drinking Tap Water', 'Someone', 'Slow Children At Play', '2:11', FALSE),
+('Slow Children At Play', 'Someone', 'Slow Children At Play', '5:32', TRUE),
+('Don''t Do Crack', 'Someone', 'Slow Children At Play', '3:06', FALSE);
