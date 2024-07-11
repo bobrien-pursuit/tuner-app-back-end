9acd4de59c74b2e8702c4bd84cd05f2ae16d8eca
Brendan O'Brien <bobrien@pursuit.org>
2024-07-11 17:07:33 -0400

    runs the getAllSongs query and converts the response to json

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -3,6 +3,9 @@ const songs = express.Router();
 const { getAllSongs } = require("../queries/songs");
 
 // INDEX
-songs.get("/", async (req, res) => {});
+songs.get("/", async (req, res) => {
+    const allSongs = await getAllSongs();
+    res.json(allSongs);
+});
 
 module.exports = songs;
