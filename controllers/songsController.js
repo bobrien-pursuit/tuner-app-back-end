a3cf64b358b0566f2f172490374576f9684ccec1
Brendan O'Brien <bobrien@pursuit.org>
2024-07-12 16:52:10 -0400

    finishes create

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -11,5 +11,24 @@ songs.get("/", async (req, res) => {
         res.status(500).json({ error: "Server error" });
     }
 });
+
+// SHOW
+
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if (song) {
+        res.status(200).json(song);
+    } else {
+        res.status(404).json({ error: "song not found"});
+    } // end if/else 
+}); // end show route
+
+// CREATE
+
+songs.post('/', async (req, res) => {
+    const song = await createSong(req.body);
+    res.json(song);
+}); // end post route
 
 module.exports = songs;
--- queries/songs.js
+++ queries/songs.js
@@ -2,11 +2,35 @@ const db = require("../db/dbConfig.js")
 
 const getAllSongs = async () => {
     try {
-      const allSongs = await db.any("SELECT * FROM songs");
+      const allSongs = await db.any("SELECT * FROM songs;");
       return allSongs;
     } catch (error) {
       return error;
     }
 };
 
-module.exports = { getAllSongs };
+const getSong = async (id) => {
+    try {
+      const song = await db.one(
+        "SELECT * FROM songs WHERE $1=id;", 
+        id
+      );
+      return song;
+   } catch (error) {
+    return error;
+  } // end try/catch
+}; // end getSong(id)
+
+const createSong = async (song) => {
+    try {
+      const newSong = await db.one(
+        "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [song.name, song.artist, song.album, song.time, song.is_favorite]
+      ); // end newSong declaration
+      return newSong;
+   } catch (error) {
+     throw error;
+   } // end try/catch/throw
+}; // end createSong()
+
+module.exports = { getAllSongs, getSong, createSong };
